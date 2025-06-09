0520921c59619efe52408d8482bf00ed72a7b5e2
nekstt <natdurka@gmail.com>
2025-06-09 22:21:09 +0200

    change descriptions and map

--- js/main.js
+++ js/main.js
@@ -223,7 +223,12 @@ const galleryModule = {
         'img/gallery/9.jpg',
         'img/gallery/10.jpg',
         'img/gallery/11.jpg',
-        'img/gallery/12.jpg'
+        'img/gallery/12.jpg',
+        'img/gallery/13.jpg',
+        'img/gallery/14.jpg',
+        'img/gallery/15.jpg',
+        'img/gallery/16.jpg',
+        'img/gallery/17.jpg'
     ],
     
     // Current image index
